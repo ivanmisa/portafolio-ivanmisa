5505f1415a38a1fc5997271204c209232c80e2d4
ivan <ivan_m_rst@hotmail.com>
2021-05-25 01:31:40 -0500

    add HashRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import MainRouter from "./MainRouter";
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <MainRouter />
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
